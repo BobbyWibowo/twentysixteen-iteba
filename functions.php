3c48a527d64b0294d8fee6dee247ff62b50051ba
Bobby Wibowo <bobbywibowo98@gmail.com>
2020-05-10 04:12:19 +0700

    Bumped style version

--- functions.php
+++ functions.php
@@ -403,7 +403,7 @@ add_action( 'wp_head', 'twentysixteen_javascript_detection', 0 );
  */
 function twentysixteen_scripts() {
 	// Version string for files that I often modify.
-	$styleVersion = '20200510-1';
+	$styleVersion = '20200510-2';
 
 	// Add custom fonts, used in the main stylesheet.
 	wp_enqueue_style( 'twentysixteen-fonts', twentysixteen_fonts_url(), array(), null );
