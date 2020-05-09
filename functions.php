9c94363d97710f7531a039ce174cc5e9f56df707
Bobby Wibowo <bobbywibowo98@gmail.com>
2020-05-10 04:34:04 +0700

    Whoops

--- functions.php
+++ functions.php
@@ -403,7 +403,7 @@ add_action( 'wp_head', 'twentysixteen_javascript_detection', 0 );
  */
 function twentysixteen_scripts() {
 	// Version string for files that I often modify.
-	$styleVersion = '20200510-3';
+	$styleVersion = '20200510-4';
 
 	// Add custom fonts, used in the main stylesheet.
 	wp_enqueue_style( 'twentysixteen-fonts', twentysixteen_fonts_url(), array(), null );
